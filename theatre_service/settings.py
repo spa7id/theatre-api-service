14631be50946ed7c2142b8203528f19b1fe9d2bc
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-12-24 14:46:32 +0200

    update setting and urls of main project

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/web/media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
